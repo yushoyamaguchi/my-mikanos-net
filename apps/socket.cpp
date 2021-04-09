3aea11ccd3e1878180224b9803461c086da67c53
YAMAMOTO Masaya <pandax381@gmail.com>
2021-04-09 15:49:42 +0900

    Add socket library

---
+++ apps/socket.cpp
@@ -0,0 +1,128 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdlib.h>
+
+#include "syscall.h"
+#include "socket.hpp"
+
+int
+ip_addr_pton (const char *p, ip_addr_t *n) {
+    char *sp, *ep;
+    int idx;
+    long ret;
+
+    sp = (char *)p;
+    for (idx = 0; idx < 4; idx++) {
+        ret = strtol(sp, &ep, 10);
+        if (ret < 0 || ret > 255) {
+            return -1;
+        }
+        if (ep == sp) {
+            return -1;
+        }
+        if ((idx == 3 && *ep != '\0') || (idx != 3 && *ep != '.')) {
+            return -1;
+        }
+        ((uint8_t *)n)[idx] = ret;
+        sp = ep + 1;
+    }
+    return 0;
+}
+
+char *
+ip_addr_ntop (const ip_addr_t *n, char *p, size_t size) {
+    uint8_t *ptr;
+
+    ptr = (uint8_t *)n;
+    snprintf(p, size, "%d.%d.%d.%d",
+        ptr[0], ptr[1], ptr[2], ptr[3]);
+    return p;
+}
+
+#ifndef __BIG_ENDIAN
+#define __BIG_ENDIAN 4321
+#endif
+#ifndef __LITTLE_ENDIAN
+#define __LITTLE_ENDIAN 1234
+#endif
+
+static int endian;
+
+static int
+byteorder(void) {
+    uint32_t x = 0x00000001;
+
+    return *(uint8_t *)&x ? __LITTLE_ENDIAN : __BIG_ENDIAN;
+}
+
+static uint16_t
+byteswap16(uint16_t v)
+{
+    return (v & 0x00ff) << 8 | (v & 0xff00 ) >> 8;
+}
+
+static uint32_t
+byteswap32(uint32_t v)
+{
+    return (v & 0x000000ff) << 24 | (v & 0x0000ff00) << 8 | (v & 0x00ff0000) >> 8 | (v & 0xff000000) >> 24;
+}
+
+uint16_t
+hton16(uint16_t h)
+{
+    if (!endian) {
+        endian = byteorder();
+    }
+    return endian == __LITTLE_ENDIAN ? byteswap16(h) : h;
+}
+
+uint16_t
+ntoh16(uint16_t n)
+{
+    if (!endian) {
+        endian = byteorder();
+    }
+    return endian == __LITTLE_ENDIAN ? byteswap16(n) : n;
+}
+
+uint32_t
+hton32(uint32_t h)
+{
+    if (!endian) {
+        endian = byteorder();
+    }
+    return endian == __LITTLE_ENDIAN ? byteswap32(h) : h;
+}
+
+uint32_t
+ntoh32(uint32_t n)
+{
+    if (!endian) {
+        endian = byteorder();
+    }
+    return endian == __LITTLE_ENDIAN ? byteswap32(n) : n;
+}
+
+int
+ioctl(int fd, int req, void *arg)
+{
+    auto [ret, err] = SyscallSocketIOCTL(fd, req, arg);
+
+    return ret;
+}
+
+int
+socket(int domain, int type, int protocol)
+{
+    auto [ret, err] = SyscallSocketOpen(domain, type, protocol);
+
+    return ret;
+}
+
+int
+soclose(int soc)
+{
+    SyscallSocketClose(soc);
+    return 0;
+}
---
+++ apps/socket.hpp
@@ -0,0 +1,91 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stddef.h>
+#include <stdint.h>
+
+#include "../kernel/net/sockio.h"
+#include "../kernel/net/if.h"
+
+#define PF_UNSPEC   0
+#define PF_LOCAL    1
+#define PF_INET     2
+
+#define AF_UNSPEC   PF_UNSPEC
+#define AF_LOCAL    PF_LOCAL
+#define AF_INET     PF_INET
+
+#define SOCK_STREAM 1
+#define SOCK_DGRAM  2
+
+#define IPPROTO_TCP 0
+#define IPPROTO_UDP 0
+
+#define INADDR_ANY ((ip_addr_t)0)
+
+typedef uint32_t ip_addr_t;
+
+struct socket {
+    int type;
+    int desc;
+};
+
+struct sockaddr {
+    unsigned short sa_family;
+    char sa_data[14];
+};
+
+struct sockaddr_in {
+    unsigned short sin_family;
+    uint16_t sin_port;
+    ip_addr_t sin_addr;
+};
+
+#define IFNAMSIZ 16
+
+struct ifreq {
+    char ifr_name[IFNAMSIZ]; /* Interface name */
+    union {
+        struct sockaddr ifr_addr;
+        struct sockaddr ifr_dstaddr;
+        struct sockaddr ifr_broadaddr;
+        struct sockaddr ifr_netmask;
+        struct sockaddr ifr_hwaddr;
+        short           ifr_flags;
+        int             ifr_ifindex;
+        int             ifr_metric;
+        int             ifr_mtu;
+//      struct ifmap    ifr_map;
+        char            ifr_slave[IFNAMSIZ];
+        char            ifr_newname[IFNAMSIZ];
+        char           *ifr_data;
+    };
+};
+
+extern int
+ip_addr_pton (const char *p, ip_addr_t *n);
+extern char *
+ip_addr_ntop (const ip_addr_t *n, char *p, size_t size);
+extern uint16_t
+hton16(uint16_t h);
+extern uint16_t
+ntoh16(uint16_t n);
+extern uint32_t
+hton32(uint32_t h);
+extern uint32_t
+ntoh32(uint32_t n);
+extern int
+ioctl(int fd, int req, void *arg);
+
+extern int
+socket(int domain, int type, int protocol);
+extern int
+soclose(int soc);
+#define close soclose
+
+#ifdef __cplusplus
+}
+#endif
